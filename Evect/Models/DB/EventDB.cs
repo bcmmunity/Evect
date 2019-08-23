886c9cf79777ff41350de6a2693218d863442d28
Artem <moranmr8@gmail.com>
2019-08-23 10:58:23 +0300

    restored eventdb methods

--- Evect/Models/DB/EventDB.cs
+++ Evect/Models/DB/EventDB.cs
@@ -18,6 +18,25 @@ namespace Evect.Models.DB
             return await Context.Events.FirstOrDefaultAsync(ev => ev.EventCode == code || ev.AdminCode == code) != null;
         }
         
+        public async Task<Event> GetEventByUserEvent(UserEvent ue)
+        {
+            return await Context.Events.FirstOrDefaultAsync(e => e.EventId == ue.EventId);
+        }
+        
+        public async Task<bool> IsAdminCode(string code)
+        {
+            return await Context.Events.FirstOrDefaultAsync(ev => ev.AdminCode == code) != null;
+        }
+        public async Task<string> GetInformationAboutEvent(long tgId)
+        {
+            User user =await Context.Users.FirstOrDefaultAsync(n=>n.TelegramId==tgId);
+            int idOfCurrentEvent = user.CurrentEventId;
+            Event currentEvent = await Context.Events.FirstOrDefaultAsync(ev => ev.EventId == idOfCurrentEvent);
+            string temp = "";
+            temp = temp + currentEvent.Info;
+            return temp;
+        }
+        
         
     }
 }
