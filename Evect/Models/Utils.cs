6688356e1fd9a656937fdef57bc7f28d06ae25cc
Verrok <moranmr8@gmail.com>
2019-08-24 17:17:09 +0300

    Added send mail method

--- Evect/Models/Utils.cs
+++ Evect/Models/Utils.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
 using System.Reflection;
+using System.Threading.Tasks;
+using MimeKit;
 
 namespace Evect.Models
 {
@@ -32,6 +34,27 @@ namespace Evect.Models
                 return false;
             }
         }
+        
+        public  async Task SendEmailAsync(string email,string subject,string message)//эмейл получателя,тема письма,текст письма
+        {
+            var emailMessage = new MimeMessage();//создет объект отправляемого сообщения
+            emailMessage.From.Add(new MailboxAddress("Администрация бота", "info@diffind.com"));//определяется отправитель
+            emailMessage.To.Add(new MailboxAddress(email));//коллекция получателей
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Plain)//тело сообщения
+            {
+                Text = message
+            };
+            using (var client = new MailKit.Net.Smtp.SmtpClient())//непосредственно само отправление сообщения
+            {
+                await client.ConnectAsync("wpl19.hosting.reg.ru", 587,false);//подключение к серверу
+                await client.AuthenticateAsync("info@diffind.com", "Diffind123!");//аутенфикация
+                await client.SendAsync(emailMessage);//отправка сообщения
+                await client.DisconnectAsync(true);//отключение
+            }
+        }
+        
+        
         
         
     }
